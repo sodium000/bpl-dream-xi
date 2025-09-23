135af08f133b34ae98ad0ca0bc34eca11add7619
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-09-23 21:07:04 +0600

    navbar added

---
+++ src/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import img from '../public/assets/logo.png'
+import '../css/navbar.css'
+
+
+export default function navbar() {
+return (
+    <div className='container mx-auto border h-[72px] mt-5 flex justify-between items-center px-3 '>
+        <div>
+            <img src={img} alt="" />
+        </div>
+        <div className='flex gap-6 justify-between items-center NavName'>
+            <div>Home</div>
+            <div>Fixture</div>
+            <div>Teams </div>
+            <div>Schedules</div>
+            <button className='border w-[125px] h-[48px] flex justify-center items-center gap-3 Navbutton rounded-xl border-gray-400'>0<span>Coin </span><img src="../public/assets/Currency.png" alt="" /></button>
+        </div>
+    </div>
+)
+}
