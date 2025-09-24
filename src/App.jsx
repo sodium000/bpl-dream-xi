fe89d41bc055130e4421d263ea9bf26ac1960e75
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-09-24 17:37:26 +0600

    json file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './navbar'
 import './App.css'
 import Banner from './banner'
+import Player from './player'
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         </div>
       </div>
     </div>
+    <Player></Player>
     </>
   )
 }
--- src/player.jsx
+++ src/player.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
+import React from "react";
+import '../css/player.css'
 
 const player = () => {
-  return (
-    <div className='container mx-auto mt-15'>
-      <div>
-        <img src="" alt="" />
-        <div>
-            <img src="" alt="" />
-            <h1>Viral Kohli</h1>
-        </div>
-        <div>
-            <div>
-                <img src="" alt="" />
-                <p>India</p>
+    return (
+        <div className="container mx-auto mt-15 grid grid-cols-3">
+            <div className="p-4 border flex flex-col border-gray-300 rounded-2xl">
+                <img
+                    src="../public/assets/Rectangle 2.png"
+                    alt=""
+                    className="plyerImage "
+                />
+                <div className="flex mt-4 gap-3 items-center">
+                    <img src="../public/assets/user 1.png" alt="" className="plyerFlag"/>
+                    <h1 className="playerName">Viral Kohli</h1>
+                </div>
+                <div className="flex justify-between items-center mt-4 ">
+                    <div className="flex gap-1.5">
+                        <img src="../public/assets/report 1.png" alt="" />
+                        <p className="playerCountry">India</p>
+                    </div>
+                    <h1 className="playerCatagory">All-Rounder</h1>
+                </div>
+                <div className="mt-6 playerRating">Rating</div>
+                <div className="flex justify-between items-center mt-2">
+                    <div className="playerBat">Left-Hand-Bat</div>
+                    <div className="playerBlow">Left-Hand-blower</div>
+                </div>
+                <div className="flex justify-between items-center mt-3">
+                    <div className="playerPrice">Price: $1500000</div>
+                    <button className="playerButton">Choose Player</button>
+                </div>
             </div>
-            <h1>All-Rounder</h1>
         </div>
-        <div>Rating</div>
-        <div>
-            <div>Left-Hand-Bat</div>
-            <div>Left-Hand-blower</div>
-        </div>
-        <div>
-            <div>Price: $1500000</div>
-            <button>Choose Player</button>
-        </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default player
+export default player;
