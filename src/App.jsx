cca8d4cfe374cc1f208124d3a5496981b7c2bf1e
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-09-24 16:17:31 +0600

    one player card added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ function App() {
     <>
     <Navbar></Navbar>
     <Banner></Banner>
+     <div className='flex justify-between items-center container mx-auto mt-15'>
+      <div className='playerH1'>Available Players</div>
+      <div className='flex items-center'>
+        <div className='playerButton1'>
+            Available
+        </div>
+        <div className='playerButton2'>
+            Selected <span>(0)</span>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
---
+++ src/player.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const player = () => {
+  return (
+    <div className='container mx-auto mt-15'>
+      <div>
+        <img src="" alt="" />
+        <div>
+            <img src="" alt="" />
+            <h1>Viral Kohli</h1>
+        </div>
+        <div>
+            <div>
+                <img src="" alt="" />
+                <p>India</p>
+            </div>
+            <h1>All-Rounder</h1>
+        </div>
+        <div>Rating</div>
+        <div>
+            <div>Left-Hand-Bat</div>
+            <div>Left-Hand-blower</div>
+        </div>
+        <div>
+            <div>Price: $1500000</div>
+            <button>Choose Player</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default player
